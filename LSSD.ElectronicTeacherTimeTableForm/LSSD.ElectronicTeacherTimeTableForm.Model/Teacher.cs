5d9ef792691bc1d5e8ef64845f88b3452a91a321
Mark Strendin <mark.strendin@gmail.com>
2020-09-05 09:24:23 -0600

    Initial commit

---
+++ LSSD.ElectronicTeacherTimeTableForm/LSSD.ElectronicTeacherTimeTableForm.Model/Teacher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSSD.ElectronicTeacherTimeTableForm.Model
+{
+    class Teacher
+    {
+    }
+}
---
+++ LSSD.ElectronicTeacherTimeTableForm/LSSD.ElectronicTeacherTimeTableForm.Model/TimeTableForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSSD.ElectronicTeacherTimeTableForm.Model
+{
+    class TimeTableForm
+    {
+        public string Teacher { get; set; }
+        public string School { get; set; }
+        public string Homeroom { get; set; }
+        public string Grades { get; set; }
+        public string Enrolment { get; set; }
+
+
+    }
+}
